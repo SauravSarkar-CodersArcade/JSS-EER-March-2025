0a6a1cd9a362a2bfcc762a69b17837c1195ac729
SauravSarkar-CodersArcade <ronniesaurav17@gmail.com>
2025-03-11 11:26:49 +0530

    Added the whiteboards as PDFs

---
+++ Arrays.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int arr1[] = {3,4,1,2,5,7,0};
+    int arr2[5] = {1,2,3,4,5};
+    int arr3[4] = {3};
+    // Default values 3,0,0,0 -> 12 bytes wasted
+    // Dynamic Memory Allocation
+    // Traverse & display values (Index)
+    int size = sizeof(arr1) / sizeof(arr1[0]);
+    printf("First element: %d\n", arr1[0]);
+    for (int i = 0; i < size; ++i) {
+        printf("%d\t", arr1[i]);
+    }
+    return 0;
+}
